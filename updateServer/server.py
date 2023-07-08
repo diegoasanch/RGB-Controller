38fdd05a4218eeedc3be8a01763a7d741f86d062
Diego Sánchez <diego@sanch.dev>
2023-07-08 14:48:53 -0300

    Doc endpoints

--- updateServer/server.py
+++ updateServer/server.py
@@ -1,13 +1,17 @@
 from flask import Flask, send_from_directory, request, abort
-
 import os
 import re
 from packaging import version
 
-def get_latest_version(directory):
-    semver_files = [f for f in os.listdir(directory)
-                    if re.fullmatch(r'v\d+\.\d+\.\d+\.bin', f)]
+app = Flask(__name__, static_folder='../build/bin')
 
+def get_latest_version(directory):
+    'Get the latest version from the build directory'
+    semver_files = [
+        f for f in os.listdir(directory)
+        # Match files using semver naming
+        if re.fullmatch(r'v\d+\.\d+\.\d+\.bin', f)
+    ]
     versions = [version.parse(f[1:-4]) for f in semver_files]
 
     if versions:
@@ -16,11 +20,9 @@ def get_latest_version(directory):
     else:
         return None
 
-
-app = Flask(__name__, static_folder='../build/bin')
-
 @app.route('/latest_version', methods=['GET'])
 def latest_version():
+    'Get the latest version from the build directory'
     latest_version = get_latest_version(app.static_folder)
     if latest_version:
         return latest_version
@@ -30,6 +32,7 @@ def latest_version():
 
 @app.route('/update', methods=['GET'])
 def update_version():
+    'Get the latest build from the build directory'
     latest_version = get_latest_version(app.static_folder)
     if latest_version:
         try:
